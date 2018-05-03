0b7dce83455ac94ccc2f2050a1e4c54b92cb2493
Sonic12040 <Daniel.g.Richmond@gmail.com>
2018-05-03 14:57:34 -0500

    Progress on Customer functionality. Show Products and Check Inventory Functions complete. Customer Prompt and CustomerQuantityQuery on docket.

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+require('console.table');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'ClassUser',
+    password: 'password',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connection success!');
+    loginPrompt();
+})
 
 function loginPrompt() {
     //Ask for UserName and Password
@@ -17,7 +31,8 @@ function loginPrompt() {
     ]).then(function(res) {
         //Switch sending the user down the appropriate view.
         if (res.user === 'customer' && res.pass === 'password') {
-            customerPrompt();
+            showProducts();
+            customerPrompt(inventory);
         }
         else if (res.user === 'manager' && res.pass === 'password') {
             managerPrompt();
@@ -31,13 +46,44 @@ function loginPrompt() {
     });
 };
 
-function customerPrompt() {
+function customerPrompt(inventory) {
     console.log('Customer Prompt Success!');
-    showProducts();
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'productChoice',
+            message: 'Type the ID of the product you would like to purchase!'
+        }
+    ]).then(function(val) {
+        let choice = parseInt(val.productChoice);
+        let product = checkInventory(choice, inventory);
+        if (product) {
+            customerQuantityPrompt();
+        } else {
+            console.log('That product is not in our inventory. Please try again.');
+            showProducts();
+            customerPrompt(inventory);
+        }
+    });
+};
+
+function checkInventory(choice, inventory) {
+    console.log('Check Inventory Success!');
+    for (let i = 0; i < array.length; i++) {
+        if (inventory[i].item_id === choice) {
+            return inventory[i];
+        };
+    };
+    return null;
 };
 
 function showProducts() {
     console.log('Show Products Success!');
+    let query = 'SELECT * from products';
+    connection.query(query, function(err, inventory) {
+        console.table(inventory);
+    });
+    return inventory;
 };
 
 function managerPrompt() {
@@ -47,7 +93,3 @@ function managerPrompt() {
 function supervisorPrompt() {
     console.log('Supervisor Prompt Success!');
 };
-
-
-
-loginPrompt();
